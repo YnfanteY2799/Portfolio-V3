9bea551b2928e658a5bb2d94c4e797b7a46fbc67
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-05-26 23:31:57 -0400

    +adds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import Navbar from "@/components/ui/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      
-
-
-    </main>
-  );
+	return (
+		<main className="min-h-screen bg-background">
+			<Navbar />
+		</main>
+	);
 }
