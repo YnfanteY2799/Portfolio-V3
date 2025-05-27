d3d351afd81203e4d54372bf146e0862cfe2506b
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-05-26 23:28:28 -0400

    +adds

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const navItems = [
+	{ name: "About", href: "#about" },
+	{ name: "Tech Stack", href: "#tech-stack" },
+	{ name: "Code Snippets", href: "#code-snippets" },
+	{ name: "GitHub", href: "#github-activity" },
+	{ name: "Projects", href: "#projects" },
+	{ name: "SoundVibes", href: "#soundvibes" },
+	{ name: "Experience", href: "#experience" },
+	{ name: "Blog", href: "#blog" },
+	{ name: "Anime", href: "/about" },
+];
