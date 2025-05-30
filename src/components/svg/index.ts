a3b568627ac87a3af62f9ead8d7e83d831581842
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-05-30 16:46:31 -0400

    Create index.ts

---
+++ src/components/svg/index.ts
@@ -0,0 +1,12 @@
+/* Socials Icons */
+
+export { default as FreelanceIcon } from "./social/FreelanceIcon.tsx";
+export { default as LeetCodeIcon } from "./social/LeetCodeIcon.tsx";
+export { default as CodewarsIcon } from "./social/CodewarsIcon.tsx";
+export { default as TwitterIcon } from "./social/TwitterIcon.tsx";
+export { default as GithubIcon } from "./social/GithubIcon.tsx";
+export { default as UpWorkIcon } from "./social/UpWorkIcon.tsx";
+export { default as DevIcon } from "./social/DevToIcon.tsx";
+export { default as MalIcon } from "./social/MalIcon.tsx";
+
+/* Socials Icons */
